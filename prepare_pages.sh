5e6194962916afb5ae3f598f93d05b0a925dd207
Youssef Elkhayat <yousseftarek@live.com>
2024-02-14 14:15:57 +0100

    Update data for the `parallel-subprocesses`

--- prepare_pages.sh
+++ prepare_pages.sh
@@ -3,6 +3,7 @@
 find ./docs -type f -name "*.log" | while read -r file; do
     echo "Processing ${file}"
     cat "${file}" | aha >"${file%.log}.html"
+    rm -rfd "${file}"
 done
 
 node docs/testsets/js/generate-tree.js
